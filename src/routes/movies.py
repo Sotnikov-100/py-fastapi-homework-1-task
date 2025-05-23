6ad9a43adc82111fdd1c10b37f557f201f62eadd
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-05-23 11:36:53 +0300

    Solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,65 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy import func
 
-from database import get_db, MovieModel
-
+from database import get_db
+from database.models import MovieModel
+from schemas.movies import MovieDetailResponseSchema, MovieListResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+@router.get("/", response_model=MovieListResponseSchema)
+async def get_movies(
+        page: int = Query(1, ge=1),
+        per_page: int = Query(10, ge=1, le=20),
+        db: AsyncSession = Depends(get_db)
+):
+    offset = (page - 1) * per_page
+
+    total_items_result = await db.execute(select(func.count(MovieModel.id)))
+    total_items = total_items_result.scalar()
+
+    if total_items == 0:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    total_pages = (total_items + per_page - 1) // per_page if total_items else 1
+
+    result = await db.execute(
+        select(MovieModel)
+        .offset(offset)
+        .limit(per_page)
+    )
+    movies = result.scalars().all()
+
+    base_url = "/theater/movies/"
+    prev_page = f"{base_url}?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"{base_url}?page={page + 1}&per_page={per_page}" if page < total_pages else None
+
+    return {
+        "movies": movies,
+        "prev_page": prev_page,
+        "next_page": next_page,
+        "total_pages": total_pages,
+        "total_items": total_items
+    }
+
+
+@router.get("/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie_details(
+        movie_id: int,
+        db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(
+        select(MovieModel)
+        .where(MovieModel.id == movie_id)
+    )
+    movie = result.scalars().first()
+
+    if not movie:
+        raise HTTPException(
+            status_code=404,
+            detail="Movie with the given ID was not found."
+        )
+
+    return movie
