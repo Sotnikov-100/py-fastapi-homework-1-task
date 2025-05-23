c2ec426f696beaab101ccd8f03b5e49044506707
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-05-23 12:29:23 +0300

    Fix name routers and import

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -7,7 +7,7 @@ from database import get_db
 from database.models import MovieModel
 from schemas.movies import MovieDetailResponseSchema, MovieListResponseSchema
 
-movie_router = APIRouter()
+router = APIRouter()
 
 
 @router.get("/movies/", response_model=MovieListResponseSchema)
